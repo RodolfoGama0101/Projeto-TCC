29cfd9c2a4b98b57fcb0cc4299cb300423a9440b
Rodolfo Gama <rodolfo.gama2006@gmail.com>
2024-04-24 18:19:29 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.gestone',
+  appName: 'gestone',
+  webDir: 'dist'
+};
+
+export default config;
